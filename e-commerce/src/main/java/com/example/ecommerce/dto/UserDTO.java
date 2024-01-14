3e2fec0cd57c16b0790d1ce1a17d10f1f31825b5
dianaG <115458163+moonchildiana@users.noreply.github.com>
2024-01-15 01:34:09 +0200

    Add files via upload

---
+++ e-commerce/src/main/java/com/example/ecommerce/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package com.example.ecommerce.dto;
+
+import com.example.ecommerce.enums.UserRole;
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private UserRole userRole;
+
+}
---
+++ e-commerce/src/main/java/com/example/ecommerce/service/user/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.ecommerce.service.user;
+
+import com.example.ecommerce.dto.SignupDTO;
+import com.example.ecommerce.dto.UserDTO;
+import com.example.ecommerce.entities.User;
+import com.example.ecommerce.enums.UserRole;
+import com.example.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDTO createUser(SignupDTO signupDTO) {
+        User user = new User();
+        user.setName(signupDTO.getName());
+        user.setEmail(signupDTO.getEmail());
+        user.setUserRole(UserRole.USER);
+        user.setPassword(new BCryptPasswordEncoder().encode(signupDTO.getPassword()));
+        User createdUser = userRepository.save(user);
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(createdUser.getId());
+        userDTO.setName(createdUser.getName());
+        userDTO.setEmail(createdUser.getEmail());
+        userDTO.setUserRole(createdUser.getUserRole());
+        return userDTO;
+    }
+
+    @Override
+    public boolean hasUserWithEmail(String email) {
+        return userRepository.findFirstByEmail(email) != null;
+    }
+}
